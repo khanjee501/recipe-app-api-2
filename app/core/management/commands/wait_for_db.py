80fd33768cd60a99712122867572361f62d22b09
Saro <57326765+khanjee501@users.noreply.github.com>
2025-04-22 21:52:19 +0500

    Added tests for wait_for_db command

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,14 @@
+"""
+Django command to wait for the Database to be available.
+"""
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """
+    Django command to wait for the database
+    """
+
+    def handle(self, *args, **options):
+        pass
