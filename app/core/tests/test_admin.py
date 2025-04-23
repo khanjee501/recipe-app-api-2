45f6863142e45ff6b2067a1b3c063f668ef457a8
Saro <57326765+khanjee501@users.noreply.github.com>
2025-04-23 16:12:48 +0500

    Added craetion of new user and its test

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -48,3 +48,10 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the creation of a user."""
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
